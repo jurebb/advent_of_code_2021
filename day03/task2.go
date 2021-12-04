4837c90f5ad1f5dde7c6fbacff717f1032bec9ad
jurebb <baban.jure@gmail.com>
2021-12-04 17:46:24 +0100

    task2

---
+++ day03/task2.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "math"
+    "os"
+    "strconv"
+    "aoc2021/utils"
+)
+
+const lineLen = 12
+
+func leastCommon(candidates []int, threshold int, totalLen int) ([]int, string) {
+    var sliceZero []int 
+    var sliceOne []int 
+    countOne := 0
+
+    // fmt.Println("co2 cands", candidates)
+    // fmt.Println("co2 thresh", threshold)
+
+    for _, val := range candidates {
+        if val < threshold {
+            sliceZero = append(sliceZero, val)
+        } else {
+            sliceOne = append(sliceOne, val - threshold)
+            countOne += 1
+        }
+    }
+
+    if totalLen == 1 {
+        if countOne > 0 {
+            return sliceOne, "1"
+        }
+        return sliceZero, "0"
+    }
+
+    if countOne >= totalLen - countOne {
+        // fmt.Println("co2 0")
+        return sliceZero, "0"
+    }
+
+    // fmt.Println("co2 1")
+    return sliceOne, "1"
+}
+
+func mostCommon(candidates []int, threshold int, totalLen int) ([]int, string) {
+    var sliceZero []int 
+    var sliceOne []int 
+    countZero := 0
+
+    // fmt.Println("o2 cands", candidates)
+    // fmt.Println("o2 thresh", threshold)
+
+    for _, val := range candidates {
+        if val >= threshold {
+            sliceOne = append(sliceOne, val - threshold)
+        } else {
+            sliceZero = append(sliceZero, val)
+            countZero += 1
+        }
+    }
+
+    if totalLen == 1 {
+        if countZero > 0 {
+            return sliceZero, "0"
+        }
+        return sliceZero, "1"
+    }
+
+    if countZero > totalLen - countZero {
+        // fmt.Println("o2 0")
+        return sliceZero, "0"
+    }
+
+    // fmt.Println("o2 1")
+    return sliceOne, "1"
+}
+
+func calculateCO2Rating(candidates []int, power int, binaryCO2Rating string) string {
+    threshold := int(math.Pow(2, float64(power)))
+
+    remainingLeastCommon, binaryVal := leastCommon(candidates, threshold, len(candidates))
+
+    if power >= 0 {
+        binaryCO2Rating = calculateCO2Rating(remainingLeastCommon, power - 1, binaryCO2Rating + binaryVal)
+    }
+
+    return binaryCO2Rating
+}
+
+func calculateO2Rating(candidates []int, power int, binaryO2Rating string) string {
+    threshold := int(math.Pow(2, float64(power)))
+
+    remainingMostCommon, binaryVal := mostCommon(candidates, threshold, len(candidates))
+
+    if power >= 0 {
+        binaryO2Rating = calculateO2Rating(remainingMostCommon, power - 1, binaryO2Rating + binaryVal)
+    }
+
+    return binaryO2Rating
+}
+
+func task2() {
+    file, _ := os.Open(os.Args[1])
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+
+    var candidates []int
+    
+    for scanner.Scan() {
+        line := scanner.Text()
+        decimal, _ := strconv.ParseInt(line, 2, 64)
+        
+        candidates = append(candidates, int(decimal))
+    }
+
+    binaryO2Rating := calculateO2Rating(candidates, lineLen - 1, "")
+    fmt.Println("o2Rating", binaryO2Rating)
+
+    binaryCO2Rating := calculateCO2Rating(candidates, lineLen - 1, "")
+    fmt.Println("co2Rating", binaryCO2Rating)
+
+    o2Rating, _ := strconv.ParseInt(binaryO2Rating, 2, 64)
+    fmt.Println("o2Rating", o2Rating)
+
+    co2Rating, _ := strconv.ParseInt(binaryCO2Rating, 2, 64)
+    fmt.Println("co2Rating", co2Rating)
+
+    fmt.Println(o2Rating * co2Rating)
+}
+
+func main() {
+    utils.Stopwatch(task2)
+}
