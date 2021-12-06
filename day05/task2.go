0521778cc21ac2d2f7b2f107ce88894ba9f6f8cd
jurebb <baban.jure@gmail.com>
2021-12-07 00:28:20 +0100

    fixing indentation

--- day05/task2.go
+++ day05/task2.go
@@ -1,151 +1,151 @@
 package main
 
 import (
-	"aoc2021/utils"
-	"bufio"
-	"fmt"
-	"math"
-	"os"
-	"strconv"
-	"strings"
+    "aoc2021/utils"
+    "bufio"
+    "fmt"
+    "math"
+    "os"
+    "strconv"
+    "strings"
 )
 
 type point struct {
-	x int
-	y int
+    x int
+    y int
 }
 
 type pointLine struct {
-	a point
-	b point
+    a point
+    b point
 }
 
 func generateAllPoints(pointLines []pointLine) (map[point]int, int) {
-	pointcounter := map[point]int{}
-	var lowerBound, upperBound, lowerBoundX, lowerBoundY, upperBoundX, upperBoundY int
+    pointcounter := map[point]int{}
+    var lowerBound, upperBound, lowerBoundX, lowerBoundY, upperBoundX, upperBoundY int
 
-	var countMoreThanTwo int
+    var countMoreThanTwo int
 
-	for _, pl := range pointLines {
-		if pl.a.x == pl.b.x {
-			if pl.a.y > pl.b.y {
-				lowerBound = pl.b.y
-				upperBound = pl.a.y
-			} else {
-				lowerBound = pl.a.y
-				upperBound = pl.b.y
-			}
+    for _, pl := range pointLines {
+        if pl.a.x == pl.b.x {
+            if pl.a.y > pl.b.y {
+                lowerBound = pl.b.y
+                upperBound = pl.a.y
+            } else {
+                lowerBound = pl.a.y
+                upperBound = pl.b.y
+            }
 
-			for i := lowerBound; i <= upperBound; i++ {
-				pointcounter[point{pl.a.x, i}] += 1
+            for i := lowerBound; i <= upperBound; i++ {
+                pointcounter[point{pl.a.x, i}] += 1
 
-				if pointcounter[point{pl.a.x, i}] == 2 {
-					countMoreThanTwo++
-				}
-			}
-		} else if pl.a.y == pl.b.y {
-			if pl.a.x > pl.b.x {
-				lowerBound = pl.b.x
-				upperBound = pl.a.x
-			} else {
-				lowerBound = pl.a.x
-				upperBound = pl.b.x
-			}
+                if pointcounter[point{pl.a.x, i}] == 2 {
+                    countMoreThanTwo++
+                }
+            }
+        } else if pl.a.y == pl.b.y {
+            if pl.a.x > pl.b.x {
+                lowerBound = pl.b.x
+                upperBound = pl.a.x
+            } else {
+                lowerBound = pl.a.x
+                upperBound = pl.b.x
+            }
 
-			for i := lowerBound; i <= upperBound; i++ {
-				pointcounter[point{i, pl.a.y}] += 1
+            for i := lowerBound; i <= upperBound; i++ {
+                pointcounter[point{i, pl.a.y}] += 1
 
-				if pointcounter[point{i, pl.a.y}] == 2 {
-					countMoreThanTwo++
-				}
-			}
-		} else if (pl.a.y - pl.b.y) == (pl.a.x - pl.b.x) {
+                if pointcounter[point{i, pl.a.y}] == 2 {
+                    countMoreThanTwo++
+                }
+            }
+        } else if (pl.a.y - pl.b.y) == (pl.a.x - pl.b.x) {
 
-			if pl.a.x > pl.b.x {
-				lowerBoundX = pl.b.x
-				upperBoundX = pl.a.x
-			} else {
-				lowerBoundX = pl.a.x
-				upperBoundX = pl.b.x
-			}
+            if pl.a.x > pl.b.x {
+                lowerBoundX = pl.b.x
+                upperBoundX = pl.a.x
+            } else {
+                lowerBoundX = pl.a.x
+                upperBoundX = pl.b.x
+            }
 
-			if pl.a.y > pl.b.y {
-				lowerBoundY = pl.b.y
-				upperBoundY = pl.a.y
-			} else {
-				lowerBoundY = pl.a.y
-				upperBoundY = pl.b.y
-			}
+            if pl.a.y > pl.b.y {
+                lowerBoundY = pl.b.y
+                upperBoundY = pl.a.y
+            } else {
+                lowerBoundY = pl.a.y
+                upperBoundY = pl.b.y
+            }
 
-			diff := int(math.Abs(float64((upperBoundY - lowerBoundY))))
+            diff := int(math.Abs(float64((upperBoundY - lowerBoundY))))
 
-			for i := 0; i <= diff; i++ {
-				pointcounter[point{lowerBoundX + i, lowerBoundY + i}] += 1
+            for i := 0; i <= diff; i++ {
+                pointcounter[point{lowerBoundX + i, lowerBoundY + i}] += 1
 
-				if pointcounter[point{lowerBoundX + i, lowerBoundY + i}] == 2 {
-					countMoreThanTwo++
-				}
-			}
-		} else if (pl.b.y - pl.a.y) == (pl.a.x - pl.b.x) {
+                if pointcounter[point{lowerBoundX + i, lowerBoundY + i}] == 2 {
+                    countMoreThanTwo++
+                }
+            }
+        } else if (pl.b.y - pl.a.y) == (pl.a.x - pl.b.x) {
 
-			if pl.a.x > pl.b.x {
-				upperBoundX = pl.a.x
-			} else {
-				upperBoundX = pl.b.x
-			}
+            if pl.a.x > pl.b.x {
+                upperBoundX = pl.a.x
+            } else {
+                upperBoundX = pl.b.x
+            }
 
-			if pl.a.y > pl.b.y {
-				lowerBoundY = pl.b.y
-				upperBoundY = pl.a.y
-			} else {
-				lowerBoundY = pl.a.y
-				upperBoundY = pl.b.y
-			}
+            if pl.a.y > pl.b.y {
+                lowerBoundY = pl.b.y
+                upperBoundY = pl.a.y
+            } else {
+                lowerBoundY = pl.a.y
+                upperBoundY = pl.b.y
+            }
 
-			diff := int(math.Abs(float64((upperBoundY - lowerBoundY))))
+            diff := int(math.Abs(float64((upperBoundY - lowerBoundY))))
 
-			for i := 0; i <= diff; i++ {
-				pointcounter[point{upperBoundX - i, lowerBoundY + i}] += 1
+            for i := 0; i <= diff; i++ {
+                pointcounter[point{upperBoundX - i, lowerBoundY + i}] += 1
 
-				if pointcounter[point{upperBoundX - i, lowerBoundY + i}] == 2 {
-					countMoreThanTwo++
-				}
-			}
-		}
-	}
+                if pointcounter[point{upperBoundX - i, lowerBoundY + i}] == 2 {
+                    countMoreThanTwo++
+                }
+            }
+        }
+    }
 
-	return pointcounter, countMoreThanTwo
+    return pointcounter, countMoreThanTwo
 }
 
 func task2() {
-	file, _ := os.Open(os.Args[1])
-	defer file.Close()
+    file, _ := os.Open(os.Args[1])
+    defer file.Close()
 
-	scanner := bufio.NewScanner(file)
+    scanner := bufio.NewScanner(file)
 
-	var pointLines []pointLine
+    var pointLines []pointLine
 
-	for scanner.Scan() {
-		line := strings.Fields(scanner.Text())
-		x1, _ := strconv.Atoi(string(strings.Split(line[0], ",")[0]))
-		y1, _ := strconv.Atoi(string(strings.Split(line[0], ",")[1]))
-		x2, _ := strconv.Atoi(string(strings.Split(line[2], ",")[0]))
-		y2, _ := strconv.Atoi(string(strings.Split(line[2], ",")[1]))
+    for scanner.Scan() {
+        line := strings.Fields(scanner.Text())
+        x1, _ := strconv.Atoi(string(strings.Split(line[0], ",")[0]))
+        y1, _ := strconv.Atoi(string(strings.Split(line[0], ",")[1]))
+        x2, _ := strconv.Atoi(string(strings.Split(line[2], ",")[0]))
+        y2, _ := strconv.Atoi(string(strings.Split(line[2], ",")[1]))
 
-		point1 := point{x1, y1}
-		point2 := point{x2, y2}
+        point1 := point{x1, y1}
+        point2 := point{x2, y2}
 
-		pl := pointLine{point1, point2}
-		pointLines = append(pointLines, pl)
-	}
+        pl := pointLine{point1, point2}
+        pointLines = append(pointLines, pl)
+    }
 
-	pointcounter, count := generateAllPoints(pointLines)
+    pointcounter, count := generateAllPoints(pointLines)
 
-	fmt.Println(pointcounter)
-	fmt.Println(count)
+    fmt.Println(pointcounter)
+    fmt.Println(count)
 
 }
 
 func main() {
-	utils.Stopwatch(task2)
+    utils.Stopwatch(task2)
 }
--- day06/task1_2.go
+++ day06/task1_2.go
@@ -1,94 +1,94 @@
 package main
 
 import (
-	"aoc2021/utils"
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
+    "aoc2021/utils"
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
 )
 
 const DAYS = 256 	// set to 80 for task 1
 
 type FishBundle struct {
-	count int
-	countYoung int
+    count int
+    countYoung int
 }
 
 func transferYoungFishes(day int, weekDayFish *map[int] FishBundle) {
-	weekDay := day % 7
-	newBirthday := (day + 7) % 7
-	youngFishesCount := (*weekDayFish)[weekDay].countYoung
+    weekDay := day % 7
+    newBirthday := (day + 7) % 7
+    youngFishesCount := (*weekDayFish)[weekDay].countYoung
 
-	(*weekDayFish)[weekDay] = FishBundle{ 
-		(*weekDayFish)[weekDay].count, 
-		0,
-	}
-	(*weekDayFish)[newBirthday] = FishBundle{ 
-		(*weekDayFish)[newBirthday].count + youngFishesCount, 
-		(*weekDayFish)[newBirthday].countYoung,
-	}
+    (*weekDayFish)[weekDay] = FishBundle{ 
+        (*weekDayFish)[weekDay].count, 
+        0,
+    }
+    (*weekDayFish)[newBirthday] = FishBundle{ 
+        (*weekDayFish)[newBirthday].count + youngFishesCount, 
+        (*weekDayFish)[newBirthday].countYoung,
+    }
 }
 
 func addUpcomingFishes(upcomingFishesCount int, day int, weekDayFish *map[int] FishBundle) {
-	birthdayOfUpcoming := (day + 9) % 7
-	(*weekDayFish)[birthdayOfUpcoming] = FishBundle{ 
-		(*weekDayFish)[birthdayOfUpcoming].count, 
-		(*weekDayFish)[birthdayOfUpcoming].countYoung + upcomingFishesCount,
-	}
+    birthdayOfUpcoming := (day + 9) % 7
+    (*weekDayFish)[birthdayOfUpcoming] = FishBundle{ 
+        (*weekDayFish)[birthdayOfUpcoming].count, 
+        (*weekDayFish)[birthdayOfUpcoming].countYoung + upcomingFishesCount,
+    }
 }
 
 func simulateDays(numDays int, newFishes []int) {
-	totalFishes := 0
-	weekDayFish := map[int] FishBundle {}
+    totalFishes := 0
+    weekDayFish := map[int] FishBundle {}
 
-	for _, fish := range newFishes {
-		bundle := FishBundle{ weekDayFish[fish].count + 1, weekDayFish[fish].countYoung }
-		weekDayFish[fish] = bundle
+    for _, fish := range newFishes {
+        bundle := FishBundle{ weekDayFish[fish].count + 1, weekDayFish[fish].countYoung }
+        weekDayFish[fish] = bundle
 
-		totalFishes++
-	}
+        totalFishes++
+    }
 
-	for day := 0; day < numDays; day++ {
-		weekDay := day % 7
-		
-		upcomingFishesCount := weekDayFish[weekDay].count
-		totalFishes += upcomingFishesCount
-		
-		addUpcomingFishes(upcomingFishesCount, day, &weekDayFish)
+    for day := 0; day < numDays; day++ {
+        weekDay := day % 7
+        
+        upcomingFishesCount := weekDayFish[weekDay].count
+        totalFishes += upcomingFishesCount
+        
+        addUpcomingFishes(upcomingFishesCount, day, &weekDayFish)
 
-		transferYoungFishes(day, &weekDayFish)
+        transferYoungFishes(day, &weekDayFish)
 
-		fmt.Println("End of day", day, ". Total fishes:", totalFishes)
-	}
+        fmt.Println("End of day", day, ". Total fishes:", totalFishes)
+    }
 }
 
 func initialize() []int {
-	file, _ := os.Open(os.Args[1])
-	defer file.Close()
+    file, _ := os.Open(os.Args[1])
+    defer file.Close()
 
-	scanner := bufio.NewScanner(file)
+    scanner := bufio.NewScanner(file)
 
-	var newbornFish []int
+    var newbornFish []int
 
-	scanner.Scan()
-	initialFishes := strings.Split(scanner.Text(), ",")
+    scanner.Scan()
+    initialFishes := strings.Split(scanner.Text(), ",")
 
-	for _, fish := range initialFishes {
-		fishNum, _ := strconv.Atoi(fish)
-		newbornFish = append(newbornFish, fishNum)
-	}
+    for _, fish := range initialFishes {
+        fishNum, _ := strconv.Atoi(fish)
+        newbornFish = append(newbornFish, fishNum)
+    }
 
-	return newbornFish
+    return newbornFish
 }
 
 func task1() {	
-	newFishes := initialize()
+    newFishes := initialize()
 
-	simulateDays(DAYS, newFishes)
+    simulateDays(DAYS, newFishes)
 }
 
 func main() {
-	utils.Stopwatch(task1)
+    utils.Stopwatch(task1)
 }
